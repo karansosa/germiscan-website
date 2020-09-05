97f8da42b84a3eedbcd5fcc353f8419365c37ac9
Karan Sosa <karansosa1995@gmail.com>
2020-09-05 13:30:03 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Germiscan</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href = "css/bootstrap.min.css" rel="stylesheet">
+<!-- <link href = "css/bootstrap.css" rel="stylesheet"> -->
+<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand ml-5" href="#">Logo</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto pr-5">
+      <li class="nav-item pr-5">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item pr-5">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item pr-5">
+        <a class="nav-link" href="#">Product</a>
+      </li>
+      <li class="nav-item pr-5">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+
+    </ul>
+
+  </div>
+</nav>

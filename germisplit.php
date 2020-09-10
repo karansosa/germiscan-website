c41d357e37c5a7357eca12b4beeca2a9998d5969
Karan Sosa <karansosa1995@gmail.com>
2020-09-10 23:26:27 +0530

    germisplit updated

--- germisplit.php
+++ germisplit.php
@@ -10,25 +10,133 @@
 
 </section>
 
-<div class="container pt-5 pb-5">
+<section class="wg" id="about" data-aos="fade-up" > 
+
+<h1 class="text-center wgh">Why Germisplit</h1>
+<p> Germiscan, a UV-C LED-based hand held sterilizer introduced by OPTOL who are pioneers in LED Lighting technology in India, 
+	is an innovative solution to all disinfection problems . UV-C light penetrates the shell of the virus,
+	 reaches its RNA and changes its structure, rendering it incapable of infecting surfaces and people. </p>
+
+</section>
+
+
+<section class="applications" data-aos="fade-up" >
+<h1 class="text-center wgh">Features</h1>
+
+<div class="container app">
+	<div class="row">
+		<div class="col-lg-4 col-md-6 col-sm-12 app-card pt-4">
+			<div class="card app-card" style="width: 18rem;">
+				<img class="mt-4"  src="img/ac.png " alt="Card image cap">
+					<div class="card-body">
+						<h5 class="card-title text-center">Fits most brands mini splits & similar A/C systems</h5>
+						<!-- <p class="card-text">99.9% sterilization in less than 6 seconds.</p> -->
+					</div>
+		    </div>
+		</div> 
+
+		<div class="col-lg-4 col-md-6 col-sm-12 app-card pt-4">
+			<div class="card app-card" style="width: 18rem;">
+					<img class="mt-4" src="img/sterilization.png" alt="Card image cap">
+						<div class="card-body">
+							<h5 class="card-title text-center">99.9 % Sterilization Efficacy</h5>
+						</div>
+			</div>
+		</div>
+
+		<div class="col-lg-4 col-md-6 col-sm-12 app-card pt-4">
+		<div class="card app-card" style="width: 18rem;">
+					<img class="mt-4" src="img/led.png" alt="Card image cap">
+						<div class="card-body">
+							<h5 class="card-title text-center">LED technology – long life , no bulb replacement</h5>
+						</div>
+		</div>
+		</div>
+
+		<div class="col-lg-4 col-md-6 col-sm-12 app-card pt-4">
+		<div class="card app-card" style="width: 18rem;">
+					<img class="mt-4" src="img/label.png" alt="Card image cap">
+						<div class="card-body">
+							<h5 class="card-title text-center">2 years product warranty</h5>
+						</div>
+			</div>
+		</div>
+
+
+	</div>
+
+	<div class="row">
+
+	</div>
+</div>
+
+</section>
+
+<section class="applications" data-aos="fade-up" >
+<h1 class="text-center wgh">Benefits</h1>
+
+<div class="container app">
+	<div class="row">
+		<div class="col-lg-4 col-md-6 col-sm-12 app-card pt-4">
+			<div class="card app-card" style="width: 18rem;">
+				<img class="mt-4"  src="img/cs.png " alt="Card image cap">
+					<div class="card-body">
+						<h5 class="card-title text-center">Inhibits mold growth inside HVAC system</h5>
+						<!-- <p class="card-text">99.9% sterilization in less than 6 seconds.</p> -->
+					</div>
+		    </div>
+		</div> 
+
+		<div class="col-lg-4 col-md-6 col-sm-12 app-card pt-4">
+			<div class="card app-card" style="width: 18rem;">
+					<img class="mt-4" src="img/air.png" alt="Card image cap">
+						<div class="card-body">
+							<h5 class="card-title text-center">Keeps air system components clean</h5>
+						</div>
+			</div>
+		</div>
+
+		<div class="col-lg-4 col-md-6 col-sm-12 app-card pt-4">
+		<div class="card app-card" style="width: 18rem;">
+					<img class="mt-4" src="img/repair.png" alt="Card image cap">
+						<div class="card-body">
+							<h5 class="card-title text-center">Saves energy/reduces maintenance costs</h5>
+						</div>
+		</div>
+		</div>
+
+		<div class="col-lg-4 col-md-6 col-sm-12 app-card pt-4">
+		<div class="card app-card" style="width: 18rem;">
+					<img class="mt-4" src="img/delivery.png" alt="Card image cap">
+						<div class="card-body">
+							<h5 class="card-title text-center">Reduces airborne microbial contaminants</h5>
+						</div>
+			</div>
+		</div>
+
+
+	</div>
+
+	<div class="row">
+
+	</div>
+</div>
+
+</section>
+
+<div class="container">
 		<div class="product-card card">
 			<div class="container-fliud">
 				<div class="p-wrapper row">
 					<div class="preview col-md-6">
 						
 						<div class="preview-pic tab-content">
-						  <div class="tab-pane active" id="pic-1"><img src="http://placekitten.com/400/252" /></div>
-						  <div class="tab-pane" id="pic-2"><img src="http://placekitten.com/400/252" /></div>
-						  <div class="tab-pane" id="pic-3"><img src="http://placekitten.com/400/252" /></div>
-						  <div class="tab-pane" id="pic-4"><img src="http://placekitten.com/400/252" /></div>
-						  <div class="tab-pane" id="pic-5"><img src="http://placekitten.com/400/252" /></div>
+						  <div class="tab-pane active" id="pic-1"><img src="img/gs.png" /></div>
+						  <div class="tab-pane" id="pic-2"><img src="img/gs-1.png" /></div>
 						</div>
 						<ul class="preview-thumbnail nav nav-tabs">
-						  <li class="active"><a data-target="#pic-1" data-toggle="tab"><img src="http://placekitten.com/200/126" /></a></li>
-						  <li><a data-target="#pic-2" data-toggle="tab"><img src="http://placekitten.com/200/126" /></a></li>
-						  <li><a data-target="#pic-3" data-toggle="tab"><img src="http://placekitten.com/200/126" /></a></li>
-						  <li><a data-target="#pic-4" data-toggle="tab"><img src="http://placekitten.com/200/126" /></a></li>
-						  <li><a data-target="#pic-5" data-toggle="tab"><img src="http://placekitten.com/200/126" /></a></li>
+						  <li class="active"><a data-target="#pic-1" data-toggle="tab"><img src="img/gs.png" /></a></li>
+						  <li><a data-target="#pic-2" data-toggle="tab"><img src="img/gs-1.png" /></a></li>
 						</ul>
 						
 					</div>
@@ -45,7 +153,7 @@
 							<span class="review-no">41 reviews</span>
 						</div>
 						<p class="product-description">Suspendisse quos? Tempus cras iure temporibus? Eu laudantium cubilia sem sem! Repudiandae et! Massa senectus enim minim sociosqu delectus posuere.</p>
-						<h4 class="price pt-5">current price: <span>4200</span></h4>
+						<h4 class="price pt-5">current price: <span>4250</span></h4>
 						<div class="action pt-5">
 							<button class="add-to-cart btn btn-default" type="button">add to cart</button>
 						</div>

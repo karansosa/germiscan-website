8db8e1629922cb70088135deeacc22c7bf7103aa
Karan Sosa <karansosa1995@gmail.com>
2020-09-10 22:52:23 +0530

    home page change

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         <img src="img/slider/slider-4.png">
     </div>
     <div class="banner-txt blk">
-	<p>UV-C LED light  </p>
+		<p>UV-C LED light  </p>
         <h2 class="blkh"> GERMISPLIT </h2>
         <p> Environmental Air Disinfection system </p>
         <p class="banner-btn "> <a href="#" > Order Now</a> </p> 
@@ -27,88 +27,113 @@
 
 </div>
 
+<section class=" pb-5 " data-aos="fade-up" >
+	<div class="container">
 
-<section class="wg" id="about" data-aos="fade-up" > 
+		<h1 class="text-center wgh">Our Products</h1>
+		<div class="row pt-5">
+			<div class="col-md-6">
+			<figure class="card card-product">
+			<div class="img-wrap"><img src="img/gs-1.png" width="500"></div>
+			<figcaption class="info-wrap">
+					<h4 class="title">Germisplit</h4>
+					<p class="desc">Some small description goes here</p>
+					<!-- <div class="rating-wrap">
+						<div class="label-rating">132 reviews</div>
+						<div class="label-rating">154 orders </div>
+					</div>  -->
+			</figcaption>
+			<div class="bottom-wrap">
+				<a href="" class="btn btn-sm btn-primary float-right product-btn">Product Detail</a>	
+				<div class="price-wrap h5">
+					<span class="price-new">4250 Rs</span> 
+				</div> 
+			</div> 
+			</figure>
+		</div>
 
-<h1 class="text-center wgh">Why Germiscan</h1>
-<p> Germiscan, a UV-C LED-based hand held sterilizer introduced by OPTOL who are pioneers in LED Lighting technology in India, 
-	is an innovative solution to all disinfection problems . UV-C light penetrates the shell of the virus,
-	 reaches its RNA and changes its structure, rendering it incapable of infecting surfaces and people. </p>
-
-</section>
-
-<section class="applications" data-aos="fade-up" >
-<h1 class="text-center wgh">Applications</h1>
-
-<div class="container app">
-	<div class="row">
-		<div class="col-lg-4 col-md-6 col-sm-12 app-card pt-4">
-			<div class="card app-card" style="width: 18rem;">
-				<img class="mt-4"  src="img/coronavirus.png " alt="Card image cap">
-					<div class="card-body">
-						<h5 class="card-title text-center">Mask , Gloves PPE kit</h5>
-						<p class="card-text">99.9% sterilization in less than 6 seconds.</p>
-					</div>
-		    </div>
-		</div> 
-
-		<div class="col-lg-4 col-md-6 col-sm-12 app-card pt-4">
-			<div class="card app-card" style="width: 18rem;">
-					<img class="mt-4" src="img/milk.png" alt="Card image cap">
-						<div class="card-body">
-							<h5 class="card-title text-center"> Grocery Shoes , Milk packets</h5>
-							<p class="card-text">99.9% sterilization in less than 6 seconds.</p>
-						</div>
+		<div class="col-md-6">
+			<figure class="card card-product">
+			<div class="img-wrap"><img src="img/gp-3.jpg" width="200"></div>
+			<figcaption class="info-wrap">
+					<h4 class="title">Germiscan </h4>
+					<p class="desc">Some small description goes here</p>
+					
+			</figcaption>
+			<div class="bottom-wrap">
+				<a href="" class="btn btn-sm btn-primary float-right product-btn">Product Detail</a>	
+				<div class="price-wrap h5">
+					<span class="price-new">6500 Rs</span> 
+				</div> 
+			</div> 
+			</figure>
 			</div>
+
+
 		</div>
 
-		<div class="col-lg-4 col-md-6 col-sm-12 app-card pt-4">
-		<div class="card app-card" style="width: 18rem;">
-					<img class="mt-4" src="img/monitor.png" alt="Card image cap">
-						<div class="card-body">
-							<h5 class="card-title text-center">Computer , Mobile</h5>
-							<p class="card-text">99.9% sterilization in less than 6 seconds.</p>
-						</div>
-		</div>
-		</div>
+	
 
-		<div class="col-lg-4 col-md-6 col-sm-12 app-card pt-4">
-		<div class="card app-card" style="width: 18rem;">
-					<img class="mt-4" src="img/delivery.png" alt="Card image cap">
-						<div class="card-body">
-							<h5 class="card-title text-center">Home Delivered Packages</h5>
-							<p class="card-text">99.9% sterilization in less than 6 seconds.</p>
-						</div>
-			</div>
-		</div>
+		
 
-		<div class="col-lg-4 col-md-6 col-sm-12 app-card pt-4">
-		<div class="card app-card" style="width: 18rem;">
-					<img class="mt-4" src="img/wallet.png" alt="Card image cap">
-						<div class="card-body text-center">
-							<h5 class="card-title">Wallet Currency notes</h5>
-							<p class="card-text">99.9% sterilization in less than 6 seconds.</p>
-						</div>
-			</div>
-		</div>
-
-		<div class="col-lg-4 col-md-6 col-sm-12 app-card pt-4">
-		<div class="card app-card" style="width: 18rem;">
-					<img class="mt-4" src="img/company.png" alt="Card image cap">
-						<div class="card-body">
-							<h5 class="card-title text-center">Working surfaces and offices</h5>
-							<p class="card-text">99.9% sterilization in less than 6 seconds.</p>
-						</div>
-			</div>
-		</div>
-	</div>
-
-	<div class="row">
+	
 
 	</div>
-</div>
-
 </section>
+
+<section class="pt-5 pb-4 hiw" data-aos="fade-up" >
+	<div class="container pt-4 pb-4">
+		<div class="row">
+		<div class="col-lg-6 col-md-6 col-sm-12">
+		
+				<h1 class="text-center wgh">How Germiscan works</h1>
+				<p class="pt-5">Germiscan is a UV-C led based handheld sterilizer, To sanitize anything First sanitize your hands & put the switch on, 
+					keep once inch distance between the light and the items and scan it, flip the items to scan on the other sides and the corners as well.  
+					99.9% sterilization in less than 6 seconds. It has an additional safety feature of a tilt sensor, which turns off the UVC light if it is 
+					tilted more than 30 degrees.  Safety switch to avoid direct radiation.</p>
+			
+
+		</div>
+
+		<div class="col-lg-6 col-md-6 col-sm-12">
+		<h1 class="text-center wgh">How Germisplit works</h1>
+		<p class="pt-5">GERMI SPLIT is a germicidal UV-C LED light system designed
+				specifically for mini-split and other ductless air-conditioning units
+				in residential and commercial applications.
+				It is an established fact that viruses and bacteria are airborne.
+				The germicidal UVC light kills microorganisms such as mold and
+				bacteria that grow inside the A/C unit as well as helps reduce the
+				airborne contaminants such as germs, viruses and allergens
+				passing through the system thereby providing continuous
+				sterilization and protection when the system is in operation.</p>		
+		</div>
+
+	
+		</div>
+	
+	</div>
+</section>
+
+
+<section class="pt-5 pb-4 hiw" data-aos="fade-up" >
+	<div class="container pt-4 pb-4">
+		<div class="row">
+		<div class="col-lg-6 col-md-6 col-sm-12">
+		<iframe width="500" height="500" src="img/germisplit.mp4" frameborder="0" 
+			allow="accelerometer;  encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+		</div>
+
+		<div class="col-lg-6 col-md-6 col-sm-12">
+		<iframe width="500" height="500" src="img/how-it-works.mp4" frameborder="0" 
+			allow="accelerometer;  encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+		</div>
+
+
+		</div>
+	
+	</div>
+</section>
+
 
 <section class="on pt-5 pb-5" data-aos="fade-up" >
 	<div class="container onc">
@@ -120,45 +145,11 @@
 
 </section>
 
-<section class="pt-5 pb-4 hiw" data-aos="fade-up" >
-	<div class="container-fluid pt-4 pb-4">
-		<div class="row">
-		<div class="col-lg-6 col-md-6 col-sm-12">
-			<div class="container">
-				<h1 class="text-center wgh">How it works</h1>
-				<p class="pt-5">Germiscan is a UV-C led based handheld sterilizer, To sanitize anything First sanitize your hands & put the switch on, 
-					keep once inch distance between the light and the items and scan it, flip the items to scan on the other sides and the corners as well.  
-					99.9% sterilization in less than 6 seconds. It has an additional safety feature of a tilt sensor, which turns off the UVC light if it is 
-					tilted more than 30 degrees.  Safety switch to avoid direct radiation.</p>
-			</div>
-
-		</div>
-
-		<div class="col-lg-6 col-md-6 col-sm-12">
-		<iframe width="500" height="500" src="img/how-it-works.mp4" frameborder="0" 
-			allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
-		</div>
-		</div>
-	
-	</div>
-</section>
-
 <section class=" testimonials pt-5" data-aos="fade-up" >
 <h1 class="text-center wgh">Product Reviews</h1>
 	<div class="container pt-5 testimonial-slider">
 		<div class="wrapper">
 				<div class="slider">
-					<!-- <div class="items">
-						<div class="text" style="width: 90%;">
-							<i class="fa fa-quote-right"> </i>
-							<p class="rp">Since it is a portable product,which can be charged like aphone, the user can carry 
-								it to the market or workplace.It can be taken in flight as well</p>
-							<div class="admin">
-								<h4 class="pt-2"> Rajiv Lunkad </h4>
-							</div>
-						</div>
-					
-					</div> -->
 
 					<div class="items" >
 						<div class="text" style="width: 90%;">
@@ -186,133 +177,7 @@
 
 
 
-<section class="faq"  data-aos="fade-up" >
 
-<div class="container pt-2 pb-5">
-	<h1 class="text-center wgh">Frequently Asked Questions</h1>
-
-	<div class="accordion pt-5" id="accordionExample">
-  <div class="card ">
-    <div class="card-header faq-card" id="headingOne">
-      <h5 class="mb-0">
-        <button class=" faq-btn btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-		How Do UVC LEDs Disinfect Pathogens ?
-        </button>
-      </h5>
-    </div>
-
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-      <div class="card-body">
-	  UVC energy is absorbed by nucleic acids inside the RNA and DNA, resulting in covalent bonds which at the right dose can render the pathogen unable to reproduce and infect. The most effective germicidal wavelength occurs with a peak between 260 nm to 270 nm, the point at which DNA absorbs UV energy the most.
-UVC energy is a commonly used sterilization technology that has been shown to inactivate a wide range of pathogens (e.g. MRSA, C. diff, E. Coli and Pseudomonas). For many years, UV mercury lamps were considered the best choice. However, lamps have several limitations, including low activity at refrigeration temperatures,
- fragile construction, long warm-up time, risk of mercury exposure, and limited UV emittance at 254 nm. In comparison, UV light-emitting diodes (UV LEDs) can be configured to emit at ideal germicidal wavelengths and provide reliable on-demand disinfection without the limitations which hamper mercury lamps.      </div>
-    </div>
-  </div>
-  <div class="card ">
-    <div class="card-header faq-header" id="headingTwo">
-      <h5 class="mb-0">
-        <button class=" faq-btn  btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-		Can it be used on humans ?
-        </button>
-      </h5>
-    </div>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-      <div class="card-body">
-	  UVC light is not supposed to be used on humans and we need to protect skin and eyes from the UVC light . 
-	  That is the reason we have provided safety features like tilt sensor and Anti UV spill design.      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header faq-header" id="headingThree">
-      <h5 class="mb-0">
-        <button class="faq-btn btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-		Can it be used on Fruits and vegetables ?
-        </button>
-      </h5>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-      <div class="card-body">
-	  Yes it can be used on fruits and vegetables and there are  enough  studies to prove that, 
-	  it increases the shelf life of fruits and vegetables      </div>
-    </div>
-  </div>
-
-  <div class="card">
-    <div class="card-header faq-header" id="headingFour">
-      <h5 class="mb-0">
-        <button class="faq-btn btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseThree">
-		What is the effective distance for disinfection ?
-        </button>
-      </h5>
-    </div>
-    <div id="collapseFour" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-      <div class="card-body">
-	  Germiscan is to be held at a distance of 1-2 cm from the object . The closer it is , the disinfection is faster      </div>
-    </div>
-  </div>
-
-  <div class="card">
-    <div class="card-header faq-header" id="headingFive">
-      <h5 class="mb-0">
-        <button class="faq-btn btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseThree">
-		What is the warranty on Germiscan ?
-        </button>
-      </h5>
-    </div>
-    <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionExample">
-      <div class="card-body">
-	  Our warranty is 1 year on the unit. You need to make sure that water should not enter the unit for the warranty to be effective      </div>
-    </div>
-  </div>
-
-  <div class="card">
-    <div class="card-header faq-header" id="headingSix">
-      <h5 class="mb-0">
-        <button class="faq-btn btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseThree">
-		Why UVC light cannot be seen ?
-        </button>
-      </h5>
-    </div>
-    <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordionExample">
-      <div class="card-body">
-	  UVC light  is invisible or very faintly visible on colored objects  , it can be seen only on a white paper .      </div>
-    </div>
-  </div>
-
-  <div class="card">
-    <div class="card-header faq-header" id="headingSeven">
-      <h5 class="mb-0">
-        <button class="faq-btn btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseThree">
-		What is the charging time of the unit ?
-        </button>
-      </h5>
-    </div>
-    <div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordionExample">
-      <div class="card-body">
-	  The charging time of the unit is 3 hours and it will work up to 3 hours of usage time after that.     </div>
-    </div>
-  </div>
-
-
-  <div class="card">
-    <div class="card-header faq-header" id="headingEight">
-      <h5 class="mb-0">
-        <button class="faq-btn btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseEight" aria-expanded="false" aria-controls="collapseThree">
-		How does the tilt sensor work ?
-        </button>
-      </h5>
-    </div>
-    <div id="collapseEight" class="collapse" aria-labelledby="headingEight" data-parent="#accordionExample">
-      <div class="card-body">
-	  If you rotate the Germiscan unit acccidentaly by more than 30 degrees on either side or totally upside down , the unit will stop working . 
-	  It will work only when the UVC light is facing downwards     </div>
-    </div>
-  </div>
-</div>
-
-</div>
-
-</section>
 
 
 
